dddc5e2ec3b88425894cd39a664b34f45b24b241
CJ <hiltycj@gmail.com>
2021-04-07 13:22:36 -0400

    Added test_syllablizer.py

--- test_syllablizer.py
+++ test_syllablizer.py
@@ -6,7 +6,7 @@ import collections
 import pytest
 from syllablizer import (
     preprocess,
-    build_next_words,
+    build_next_syllables,
     generate_sentence,
     generate_text,
 )
@@ -15,6 +15,31 @@ test_syllablizer
 
 for word in tests:
 
-WORD_CASES
+WORD_CASES = [
+    ("passive",["pas","sive"])
+    ("influential",["in","flu","en","tial"])
+    ("provisions",["pro","vi","sions"])
+    ("partners",["part","ners"])
+    ("bluebell",["blue","bell"])
+    ("rationale",["ra","tion","ale"])
+    ("Coca-Cola",["co","ca","co","la"])
+    ("account",["ac","count"])
+    ("banana",["ba","na","na"])
+    ("flakes",["flakes"])
+    ("tiny",["ti","ny"])
+    ("draft",["draft"])
+    ("Africa",["af","ri","ca"])
+    ("concrete",["con","crete"])
+    ("described",["de","scribed"])
+    ("recognition",["rec","og","nit","ion"])
+    ("niche",["niche"])
+    ("condominiums",["con","do","min","i","ums"])
+    ("recorded",["rec","ord","ed"])
+    ("September",["sep","tem","ber"])
+    ("ceremony",["cer","e","mo","ny"])
+    ("student",["stu","dent"])
+    ("apple",["ap","ple"])
+    
+]
 
 assert(syllablize(WORD_CASES[0]) == WORD_CASES[1])
